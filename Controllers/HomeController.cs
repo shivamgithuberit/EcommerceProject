a5a5b227e9d6d6d33281ec48025ac6c0b7c5b6d0
shivamgithubrit <smaurya07sitm@gmail.com>
2024-08-12 16:56:58 +0530

    Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace EcommerceProject.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
